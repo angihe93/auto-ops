98c5434bedd7ab9a38b14a93356faaa5cbdb5c6f
angelhe <angelhe903@gmail.com>
2022-07-15 22:49:33 -0400

    change auth

--- app.py
+++ app.py
@@ -128,6 +128,25 @@ def gfg():
 def makecalevents():
     # see the tasks where time is scheduled
     # click on tasks to send cal events for, with option to edit
+
+
+  # # Load credentials from the session.
+  # credentials = google.oauth2.credentials.Credentials(
+  #     **flask.session['credentials'])
+  #
+  # drive = googleapiclient.discovery.build(
+  #     API_SERVICE_NAME, API_VERSION, credentials=credentials)
+  #
+  # files = drive.files().list().execute()
+  #
+  # # Save credentials back to session in case access token was refreshed.
+  # # ACTION ITEM: In a production app, you likely want to save these
+  # #              credentials in a persistent database instead.
+  # flask.session['credentials'] = credentials_to_dict(credentials)
+  #
+  # return flask.jsonify(**files)
+
+
     SCOPES = ['https://www.googleapis.com/auth/calendar']
 
     creds = None
@@ -151,7 +170,8 @@ def makecalevents():
             # creds = flow.run_local_server(port=8080)
             flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
                 'google-credentials.json', scopes=SCOPES)
-            flow.redirect_uri = 'https://auto-ops.herokuapp.com/makecalevents'
+            flow.redirect_uri = flask.url_for('oauth2callback',_external=True)
+            # flow.redirect_uri = 'https://auto-ops.herokuapp.com/addevents' #/makecalevents
 
             authorization_url, state = flow.authorization_url(
                 # Enable offline access so that you can refresh an access token without
@@ -159,11 +179,92 @@ def makecalevents():
                 access_type='offline',
                 # Enable incremental authorization. Recommended as a best practice.
                 include_granted_scopes='true')
+
+        flask.session['state'] = state
         return redirect(authorization_url)
         # Save the credentials for the next run
         # with open('token.json', 'w') as token:
         #     token.write(creds.to_json())
 
+    # try:
+    #     service = build('calendar', 'v3', credentials=creds)
+    #
+    #     # Call the Calendar API
+    #     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    #     print('Getting the upcoming 10 events')
+    #     events_result = service.events().list(calendarId='primary', timeMin=now,
+    #                                           maxResults=10, singleEvents=True,
+    #                                           orderBy='startTime').execute()
+    #     events = events_result.get('items', [])
+    #
+    #     if not events:
+    #         print('No upcoming events found.')
+    #         return
+    #
+    #     # Prints the start and name of the next 10 events
+    #     for event in events:
+    #         start = event['start'].get('dateTime', event['start'].get('date'))
+    #         print(start, event['summary'])
+    #
+    #     event = {
+    #       'summary': 'Google I/O 2015',
+    #       'location': '800 Howard St., San Francisco, CA 94103',
+    #       'description': 'A chance to hear more about Google\'s developer products.',
+    #       'start': {
+    #         'dateTime': '2022-07-13T09:00:00-07:00',
+    #         'timeZone': 'America/Los_Angeles',
+    #       },
+    #       'end': {
+    #         'dateTime': '2022-07-28T17:00:00-07:00',
+    #         'timeZone': 'America/Los_Angeles',
+    #       },
+    #       # 'recurrence': [
+    #       #   'RRULE:FREQ=DAILY;COUNT=2'
+    #       # ],
+    #       'attendees': [
+    #         {'email': 'lpage@example.com'},
+    #         {'email': 'sbrin@example.com'},
+    #       ],
+    #       'reminders': {
+    #         'useDefault': False,
+    #         'overrides': [
+    #           {'method': 'email', 'minutes': 24 * 60},
+    #           {'method': 'popup', 'minutes': 10},
+    #         ],
+    #       },
+    #     }
+    #
+    #     event = service.events().insert(calendarId='ah3354@columbia.edu', body=event).execute()
+    #     print('Event created: %s' % (event.get('htmlLink')))
+    #
+    # except HttpError as error:
+    #     print('An error occurred: %s' % error)
+
+
+    # return render_template("makecalevents.html")
+
+
+@app.route('/oauth2callback', methods =["GET", "POST"])
+def addevents():
+    state = flask.session['state']
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+          CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = flask.url_for('addevents', _external=True)
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+  # Store credentials in the session.
+  # ACTION ITEM: In a production app, you likely want to save these
+  #              credentials in a persistent database instead.
+    credentials = flow.credentials
+    flask.session['credentials'] = credentials_to_dict(credentials)
+
+    return flask.redirect(flask.url_for('test_api_request'))
+
+@app.route('/test_api_request', methods =["GET", "POST"])
+def test_api_request():
     try:
         service = build('calendar', 'v3', credentials=creds)
 
@@ -217,9 +318,6 @@ def makecalevents():
 
     except HttpError as error:
         print('An error occurred: %s' % error)
-
-
-    return render_template("makecalevents.html")
 
 
 if __name__ == "__main__":
