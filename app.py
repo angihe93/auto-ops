13da241c386040ed1c2544a3763815bd39e56f5a
angelhe <angelhe903@gmail.com>
2022-07-16 15:21:41 -0400

    small change

--- app.py
+++ app.py
@@ -392,80 +392,80 @@ def makecalevents():
     # return render_template("makecalevents.html")
 
 
-@app.route('/oauth2callback', methods =["GET", "POST"])
-def addevents():
-    state = flask.session['state']
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-          CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
-    flow.redirect_uri = flask.url_for('addevents', _external=True)
-
-    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
-    authorization_response = flask.request.url
-    flow.fetch_token(authorization_response=authorization_response)
-
-  # Store credentials in the session.
-  # ACTION ITEM: In a production app, you likely want to save these
-  #              credentials in a persistent database instead.
-    credentials = flow.credentials
-    flask.session['credentials'] = credentials_to_dict(credentials)
-
-    return flask.redirect(flask.url_for('test_api_request'))
-
-@app.route('/test_api_request', methods =["GET", "POST"])
-def test_api_request():
-    try:
-        service = build('calendar', 'v3', credentials=creds)
-
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
-
-        if not events:
-            print('No upcoming events found.')
-            return
-
-        # Prints the start and name of the next 10 events
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
-
-        event = {
-          'summary': 'Google I/O 2015',
-          'location': '800 Howard St., San Francisco, CA 94103',
-          'description': 'A chance to hear more about Google\'s developer products.',
-          'start': {
-            'dateTime': '2022-07-13T09:00:00-07:00',
-            'timeZone': 'America/Los_Angeles',
-          },
-          'end': {
-            'dateTime': '2022-07-28T17:00:00-07:00',
-            'timeZone': 'America/Los_Angeles',
-          },
-          # 'recurrence': [
-          #   'RRULE:FREQ=DAILY;COUNT=2'
-          # ],
-          'attendees': [
-            {'email': 'lpage@example.com'},
-            {'email': 'sbrin@example.com'},
-          ],
-          'reminders': {
-            'useDefault': False,
-            'overrides': [
-              {'method': 'email', 'minutes': 24 * 60},
-              {'method': 'popup', 'minutes': 10},
-            ],
-          },
-        }
-
-        event = service.events().insert(calendarId='ah3354@columbia.edu', body=event).execute()
-        print('Event created: %s' % (event.get('htmlLink')))
-
-    except HttpError as error:
-        print('An error occurred: %s' % error)
+# @app.route('/oauth2callback', methods =["GET", "POST"])
+# def addevents():
+#     state = flask.session['state']
+#     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+#           CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+#     flow.redirect_uri = flask.url_for('addevents', _external=True)
+#
+#     # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+#     authorization_response = flask.request.url
+#     flow.fetch_token(authorization_response=authorization_response)
+#
+#   # Store credentials in the session.
+#   # ACTION ITEM: In a production app, you likely want to save these
+#   #              credentials in a persistent database instead.
+#     credentials = flow.credentials
+#     flask.session['credentials'] = credentials_to_dict(credentials)
+#
+#     return flask.redirect(flask.url_for('test_api_request'))
+#
+# @app.route('/test_api_request', methods =["GET", "POST"])
+# def test_api_request():
+#     try:
+#         service = build('calendar', 'v3', credentials=creds)
+#
+#         # Call the Calendar API
+#         now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+#         print('Getting the upcoming 10 events')
+#         events_result = service.events().list(calendarId='primary', timeMin=now,
+#                                               maxResults=10, singleEvents=True,
+#                                               orderBy='startTime').execute()
+#         events = events_result.get('items', [])
+#
+#         if not events:
+#             print('No upcoming events found.')
+#             return
+#
+#         # Prints the start and name of the next 10 events
+#         for event in events:
+#             start = event['start'].get('dateTime', event['start'].get('date'))
+#             print(start, event['summary'])
+#
+#         event = {
+#           'summary': 'Google I/O 2015',
+#           'location': '800 Howard St., San Francisco, CA 94103',
+#           'description': 'A chance to hear more about Google\'s developer products.',
+#           'start': {
+#             'dateTime': '2022-07-13T09:00:00-07:00',
+#             'timeZone': 'America/Los_Angeles',
+#           },
+#           'end': {
+#             'dateTime': '2022-07-28T17:00:00-07:00',
+#             'timeZone': 'America/Los_Angeles',
+#           },
+#           # 'recurrence': [
+#           #   'RRULE:FREQ=DAILY;COUNT=2'
+#           # ],
+#           'attendees': [
+#             {'email': 'lpage@example.com'},
+#             {'email': 'sbrin@example.com'},
+#           ],
+#           'reminders': {
+#             'useDefault': False,
+#             'overrides': [
+#               {'method': 'email', 'minutes': 24 * 60},
+#               {'method': 'popup', 'minutes': 10},
+#             ],
+#           },
+#         }
+#
+#         event = service.events().insert(calendarId='ah3354@columbia.edu', body=event).execute()
+#         print('Event created: %s' % (event.get('htmlLink')))
+#
+#     except HttpError as error:
+#         print('An error occurred: %s' % error)
 
 
 if __name__ == "__main__":
